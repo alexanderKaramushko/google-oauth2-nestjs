e0dd38b0c8239a293bc698541ac51b912880b7ba
a.karamushko <a.karamushko@tbank.ru>
2026-09-05 15:39:20 +0400

    базовый CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-version=$1
+tag=$1
 
-docker build --platform linux/amd64 -t melkor73/goals-auth:$version -t melkor73/goals-auth:latest $PWD
+docker build --platform linux/amd64 -t melkor73/goals-auth:$tag -t melkor73/goals-auth:latest $PWD
 
-docker push melkor73/goals-auth:$version
+docker push melkor73/goals-auth:$tag
 docker push melkor73/goals-auth:latest
